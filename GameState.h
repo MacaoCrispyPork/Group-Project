a0898c3cbec60b3b171a1fc34499b45ada2e01e8
Jason Liang <jasonliang1721@gmail.com>
2025-05-14 18:11:26 +0930

    Menu

---
+++ GameState.h
@@ -0,0 +1,12 @@
+#ifndef GAMESTATE_H
+#define GAMESTATE_H
+
+enum class gameState
+{
+    Main_Menu,
+    Weapons_Menu,
+    Game,
+    Death,
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,10 +1,81 @@
+#include <SFML/Graphics.hpp>
 #include "Game.h"
+#include "Button.h"
+#include "GameState.h"
 
 int main()
 {
     srand(time(0));
-    Game g(500, 500, "Slayer");
-    g.run();
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Slayer Menu");
+
+    sf::Font font;
+    if (!font.loadFromFile("ARIAL.TTF"))
+    {
+        return -1;
+    }
+
+    gameState state = gameState::Main_Menu;
+
+    // Buttons
+    Button playBtn({200, 50}, {300, 200}, "Play!", font);
+    Button quitBtn({200, 50}, {300, 300}, "Exit :(", font);
+    Button wBow({200, 50}, {300, 200}, "Bow", font);
+    Button wSword({200, 50}, {300, 300}, "Sword", font);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                sf::Vector2i mouse(event.mouseButton.x, event.mouseButton.y);
+
+                if (state == gameState::Main_Menu)
+                {
+                    if (playBtn.isClicked(mouse))
+                    {
+                        state = gameState::Weapons_Menu;
+                    }
+                    else if (quitBtn.isClicked(mouse))
+                    {
+                        window.close();
+                    }
+                }
+
+                else if (state == gameState::Weapons_Menu)
+                {
+                    if (wBow.isClicked(mouse) || wSword.isClicked(mouse))
+                    {
+                        // You can use a flag here to distinguish Option 1 vs 2
+                        window.close(); // Close menu window
+                        Game g(500, 500, "Slayer");
+                        g.run();  // Start the game
+                        return 0; // Exit after game finishes
+                    }
+                }
+            }
+        }
+
+        window.clear();
+
+        if (state == gameState::Main_Menu)
+        {
+            playBtn.draw(window);
+            quitBtn.draw(window);
+        }
+        else if (state == gameState::Weapons_Menu)
+        {
+            wBow.draw(window);
+            wSword.draw(window);
+        }
+
+        window.display();
+    }
 
     return 0;
 }
